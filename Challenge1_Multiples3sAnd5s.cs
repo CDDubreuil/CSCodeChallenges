e8255088d9bbc89429eebf864526c07c0e8f1d93
Chanel Dubreuil <cddubreuil@outlook.com>
2023-07-10 20:14:21 -0500

    Made program to find integers that are multiples of 3 and 5.

---
+++ Challenge1_Multiples3sAnd5s.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Schema;
+
+namespace Multiples3sAnd5s
+{
+    internal class Challenge1_Multiples3sAnd5s
+    {
+        static void Main(string[] args)
+        {
+
+         int sum = 0;
+
+
+
+          for (int x = 0; x < 1000; x++)
+            {
+
+                
+                if ( x % 3 == 0 || x % 5 == 0)
+                {
+                    sum += x;
+
+                    Console.WriteLine(sum);
+                }
+                
+
+            }
+
+
+
+        }
+        }
+    }
